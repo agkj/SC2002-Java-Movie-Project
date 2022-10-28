2e60efe3f1b1d38369976c53f8a000fa56659ce6
bchewzy <bchewzy@gmail.com>
2022-10-28 23:07:56 +0800

    added updateMovie

--- src/Admin/MovieListingApp.java
+++ src/Admin/MovieListingApp.java
@@ -93,6 +93,8 @@ public class MovieListingApp extends AppInterface {
                 System.out.println("Please enter an ID.");
         }
 
+        newMovie.setMovieId(id);
+
         //// TITLE
         System.out.print("Enter Movie Title: ");
         String title = sc.nextLine();
@@ -152,7 +154,7 @@ public class MovieListingApp extends AppInterface {
         MovieGenre movieGenre = MovieGenre.values()[genre-1];
         newMovie.setGenre(movieGenre);
 
-        //// Showing Status
+        //// SHOWING STATUS
         System.out.println("Select Showing Status: ");
 
         // Loop through all ShowingStatus options
@@ -245,6 +247,212 @@ public class MovieListingApp extends AppInterface {
 
     //// (3) UPDATE LISTING
     public void updateMovie() {
+        try {
+            for(int i=0; i < movieFiles.length; i++) {
+                Movie curr = (Movie) Serializer.deSerialize(path + "\\" + movieFiles[i].getName());
+                System.out.println((i+1) + ") " + curr.getTitle());
+            }
+
+            System.out.print("Enter Movie Index to Update: ");
+            int index = sc.nextInt()-1;
+
+            File selected = movieFiles[index];
+            Movie movieToUpdate = (Movie) Serializer.deSerialize(path + "\\" + movieFiles[index].getName());
+            ArrayList<String> cast = movieToUpdate.getCast();
+
+            int input;
+
+            do {
+                System.out.println("Select Field to Update: ");
+
+                //System.out.println("1) ID");
+                System.out.println("1) Title");
+                System.out.println("2) Director");
+                System.out.println("3) Genre");
+                System.out.println("4) Showing Status");
+                System.out.println("5) Runtime");
+                System.out.println("6) Content Rating");
+                System.out.println("7) Add Cast");
+                System.out.println("8) Remove Cast");
+
+                System.out.println("\n0) Save and Return");
+                System.out.println("-1) Discard and Return");
+
+                input = sc.nextInt();
+
+                switch(input) {
+                    case -1:
+                        break;
+                    case 0:
+                        // Try to save file
+                        try {
+                            Serializer.serialize(root + "\\data\\movies\\" + selected.getName(), movieToUpdate);
+
+                            // Reload Movies
+                            this.load();
+
+                            System.out.println("\n------- SUCCESS: UPDATED MOVIE LISTING -------\n");
+                            System.out.println(movieToUpdate);
+                        } catch (IOException e) {
+                            System.out.println("\n------- ERROR: PLEASE TRY AGAIN -------\n");
+                            e.printStackTrace();
+                        }
+
+                        break;
+                    case 1:
+                        //// TITLE
+                        System.out.print("Enter Updated Movie Title: ");
+                        String title = sc.nextLine();
+
+                        while(title.isEmpty()) {
+                            title = sc.nextLine();
+
+                            if(title.isEmpty())
+                                System.out.println("Please enter a title.");
+                        }
+
+                        movieToUpdate.setTitle(title);
+
+                        break;
+                    case 2:
+                        //// DIRECTOR
+                        System.out.print("Enter Updated Director: ");
+                        String director = sc.nextLine();
+
+                        while(director.isEmpty()) {
+                            director = sc.nextLine();
+
+                            if(director.isEmpty())
+                                System.out.println("Please enter a director.");
+                        }
+
+                        movieToUpdate.setDirector(director);
+
+                        break;
+                    case 3:
+                        //// GENRE
+                        System.out.print("Select Updated Genre: ");
+
+                        // Loop through all MovieGenre options
+                        for(int i=0; i < MovieGenre.values().length; i++) {
+                            System.out.println(i+1 + ") " + MovieGenre.values()[i]);
+                        }
+
+                        int genre = sc.nextInt();
+
+                        while(genre < 1 || genre > MovieGenre.values().length) {
+                            genre = sc.nextInt();
+
+                            if(genre < 1 || genre > MovieGenre.values().length)
+                                System.out.println("Please enter a valid genre option.");
+                        }
+
+                        MovieGenre movieGenre = MovieGenre.values()[genre-1];
+                        movieToUpdate.setGenre(movieGenre);
+
+                        break;
+                    case 4:
+                        //// SHOWING STATUS
+                        System.out.println("Select Updated Showing Status: ");
+
+                        // Loop through all ShowingStatus options
+                        for(int i=0; i < ShowingStatus.values().length; i++) {
+                            System.out.println(i+1 + ") " + ShowingStatus.values()[i]);
+                        }
+
+                        int status = sc.nextInt();
+
+                        while(status < 1 || status > ShowingStatus.values().length) {
+                            System.out.println("Please enter a valid showing status option.");
+
+                            status = sc.nextInt();
+                        }
+
+                        ShowingStatus showStatus = ShowingStatus.values()[status-1];
+                        movieToUpdate.setShowingStatus(showStatus);
+
+                        break;
+                    case 5:
+                        //// RUNTIME
+                        System.out.print("Enter Updated Runtime Duration: ");
+                        while(!sc.hasNextInt())
+                            System.out.println("Please enter a valid runtime duration");
+                        movieToUpdate.setRuntime(sc.nextInt());
+
+                        break;
+                    case 6:
+                        //// CONTENT RATING (PG, etc)
+                        System.out.println("Select Updated Content Rating: ");
+
+                        // Loop through all Content Rating options
+                        for(int i=0; i < ContentRating.values().length; i++)
+                            System.out.println(i+1 + ") " + ContentRating.values()[i]);
+
+                        int contentRating = sc.nextInt();
+
+                        while(contentRating < 1 || contentRating > ContentRating.values().length) {
+                            System.out.println("Please enter a valid content rating option.");
+                        }
+
+                        ContentRating rating = ContentRating.values()[contentRating-1];
+                        movieToUpdate.setContentRating(rating);
+
+                        break;
+                    case 7:
+                        ////  CAST
+                        // Print out current cast saved
+                        System.out.println("Current Cast: ");
+                        for(int i=0; i < cast.size(); i++) {
+                            System.out.println((i+1) + ") " + cast.get(i));
+                        }
+
+                        int j = cast.size();
+
+                        String castName = sc.nextLine();
+
+                        while(!castName.equals("-1")) {
+                            System.out.print("Enter name of cast member " + (j++) + " (type -1 to end list): ");
+
+                            // Validation
+                            if(!castName.isEmpty())
+                                cast.add(castName);     // Add new Member
+
+                            castName = sc.nextLine();
+                        }
+
+                        movieToUpdate.setCast(cast);    // Update Cast ArrayList for Movie Instance
+
+                        break;
+                    case 8:
+                        //// REMOVE CAST
+
+                        // Print out current cast saved
+                        System.out.println("Current Cast: ");
+                        for(int i=0; i < cast.size(); i++) {
+                            System.out.println((i+1) + ") " + cast.get(i));
+                        }
+
+                        System.out.println("Enter Cast Member Index to Delete: ");
+
+                        while(!sc.hasNextInt() && sc.nextInt() < cast.size() && sc.nextInt() > 1)
+                            System.out.println("Please enter a valid index.");
+
+
+                        cast.remove(sc.nextInt()-1);      // Remove Member
+
+                        movieToUpdate.setCast(cast);    // Update Cast ArrayList for Movie Instance
+
+                        break;
+                    default:
+                        runInterface();
+                        break;
+                }
+
+            } while (input > 0);
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
         runInterface();
     }

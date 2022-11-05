59b1b61849f292fb9e245d69ab779d52fb6e7a92
agkj <alger.goh99@gmail.com>
2022-11-05 22:42:30 +0800

    Deleted booby bug file

--- src/MovieGoer/MovieGoerTop5.java
+++ src/MovieGoer/MovieGoerTop5.java
@@ -16,85 +16,56 @@ import Entities.Movie;
 import Util.Serializer;
 
 public class MovieGoerTop5 extends MovieListingApp {
-	
-	
+
 	public MovieGoerTop5(AppInterface prevApp) {
 		super(prevApp);
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public void load() {
-        // Try to read all movie .dat files in movie directory
-        path = new File(System.getProperty("user.dir") + "\\data\\movies");
+		// Try to read all movie .dat files in movie directory
+		path = new File(System.getProperty("user.dir") + "\\data\\movies");
 
-        // Store all movie .dat files
-        movieFiles = path.listFiles();
-    }
-
-	
-	public void movieSort() {
-		
-		try {
-            // Read all available Movies
-			Map<String, Double> mapOfMovies = new HashMap<>();
-            if(movieFiles != null) {
-            	
-                for(int i=0; i < movieFiles.length; i++) {
-                    Movie curr = (Movie) Serializer.deSerialize(path + "\\" + movieFiles[i].getName());
-                    mapOfMovies.put(curr.getTitle(), curr.getOverallRating());
-                    
-
-                    
-                }
-            }
-            List<Map.Entry<String, Double>> list = new ArrayList<>(mapOfMovies.entrySet());
-            
-            
-            Collections.sort(list,new Comparator<Map.Entry<String, Double>>() {
-            	
-            	
-            	
-            	public int compare (Map.Entry<String, Double> e1, Map.Entry<String,Double> e2) {
-            		
-            		return Double.compare(e1.getValue(), e2.getValue());
-            	}
-            	
-            	
-			});
-            
-            
-            System.out.println("*********Here are the top 5 movies!*********");
-            for(Map.Entry<String, Double> e:list) {
-            	System.out.println(e.getKey() +" Rating: "+ e.getValue());
-            }
-            System.out.println("*********************************************");
-            
-           
-            
-            
-
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-		
-		
-		
-		
-		
-		
+		// Store all movie .dat files
+		movieFiles = path.listFiles();
 	}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+	public void movieSort() {
+
+		try {
+			// Read all available Movies
+			Map<String, Double> mapOfMovies = new HashMap<>();
+			if (movieFiles != null) {
+
+				for (int i = 0; i < movieFiles.length; i++) {
+					Movie curr = (Movie) Serializer.deSerialize(path + "\\" + movieFiles[i].getName());
+					mapOfMovies.put(curr.getTitle(), curr.getOverallRating());
+
+				}
+			}
+			List<Map.Entry<String, Double>> list = new ArrayList<>(mapOfMovies.entrySet());
+
+			Collections.sort(list, new Comparator<Map.Entry<String, Double>>() {
+
+				public int compare(Map.Entry<String, Double> e1, Map.Entry<String, Double> e2) {
+
+					return Double.compare(e1.getValue(), e2.getValue());
+				}
+
+			});
+
+			System.out.println("*********Here are the top 5 movies!*********");
+			for (Map.Entry<String, Double> e : list) {
+				System.out.println(e.getKey() + " Rating: " + e.getValue());
+			}
+			System.out.println("*********************************************");
+
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 //		try {
 //            // Read all available Movies
 //            if(movieFiles != null) {
@@ -119,7 +90,5 @@ public class MovieGoerTop5 extends MovieListingApp {
 //		
 //		
 //	}
-	
-	
 
 }

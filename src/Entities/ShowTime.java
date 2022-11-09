50b93316917f363c099f7c73745c11f409b94d19
agkj <alger.goh99@gmail.com>
2022-11-09 13:04:05 +0800

    Revert "Revert "Some updates""
    This reverts commit 0ac9d39ed6f24fc8320f59ecf100a0aa42fd7963.

--- src/Entities/ShowTime.java
+++ src/Entities/ShowTime.java
@@ -5,107 +5,109 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 
 public class ShowTime implements Serializable {
-    @Serial
-    private static final long serialVersionUID = 2002;
+	@Serial
+	private static final long serialVersionUID = 2002;
 
-    private String showtimeID;
-    private String cinemaID;
-    private String cineplexID;
-    private LocalDateTime showDateTime;
-    private ShowTimeStatus showTimeStatus;
-    private Seat[][] showTimeLayout;
+	private String showtimeID;
+	private String cinemaID;
+	private String cineplexID;
+	private LocalDateTime showDateTime;
+	private ShowTimeStatus showTimeStatus;
+	private Seat[][] showTimeLayout;
 
-    private int numOfAvailSeats;
+	private int numOfAvailSeats;
 
-    public ShowTime() {
-        this.showTimeStatus = ShowTimeStatus.Available;
-    }
+	public ShowTime() {
+		this.showTimeStatus = ShowTimeStatus.Available;
+	}
 
-    public String getCinemaID() {
-        return cinemaID;
-    }
+	public String getCinemaID() {
+		return cinemaID;
+	}
 
-    public void setCinemaID(String cinemaID) {
-        this.cinemaID = cinemaID;
-    }
+	public void setCinemaID(String cinemaID) {
+		this.cinemaID = cinemaID;
+	}
 
-    public String getShowtimeID() {
-        return showtimeID;
-    }
+	public String getShowtimeID() {
+		return showtimeID;
+	}
 
-    public void setShowtimeID(String showtimeID) {
-        this.showtimeID = showtimeID;
-    }
+	public void setShowtimeID(String showtimeID) {
+		this.showtimeID = showtimeID;
+	}
 
-    public String getCineplexID() {
-        return cineplexID;
-    }
+	public String getCineplexID() {
+		return cineplexID;
+	}
 
-    public void setCineplexID(String cineplexID) {
-        this.cineplexID = cineplexID;
-    }
+	public void setCineplexID(String cineplexID) {
+		this.cineplexID = cineplexID;
+	}
 
-    public LocalDateTime getShowDateTime() {
-        return showDateTime;
-    }
+	public LocalDateTime getShowDateTime() {
+		return showDateTime;
+	}
 
-    public void setShowDateTime(LocalDateTime showDateTime) {
-        this.showDateTime = showDateTime;
-    }
+	public void setShowDateTime(LocalDateTime showDateTime) {
+		this.showDateTime = showDateTime;
+	}
 
-    public ShowTimeStatus getShowTimeStatus() {
-        return showTimeStatus;
-    }
+	public ShowTimeStatus getShowTimeStatus() {
+		return showTimeStatus;
+	}
 
-    public void setShowTimeStatus(ShowTimeStatus showTimeStatus) {
-        this.showTimeStatus = showTimeStatus;
-    }
+	public void setShowTimeStatus(ShowTimeStatus showTimeStatus) {
+		this.showTimeStatus = showTimeStatus;
+	}
 
-    public Seat[][] getShowTimeLayout() {
-        return showTimeLayout;
-    }
+	public Seat[][] getShowTimeLayout() {
+		return showTimeLayout;
+	}
 
-    // Print Seating Layout
-    public void showLayout() {
-        char rowNum = 65;   // start at A (65), ends at Z (90)
+	// Print Seating Layout
+	public void showLayout() {
+		char rowNum = 65; // start at A (65), ends at Z (90)
 
-        for(int i=0; i < showTimeLayout.length; i++) {
-            System.out.print(rowNum++ + " | ");
+		for (int i = 0; i < showTimeLayout.length; i++) {
+			System.out.print(rowNum++ + " | ");
 
-            for(int j=0; j < showTimeLayout[0].length; j++) {       // showTimeLayout[0] can be any index, just need to get the number of cols
-                // Print seat status
-                System.out.print(" ["+ showTimeLayout[i][j].getSeatStatus() +"] ");
-            }
+			for (int j = 0; j < showTimeLayout[0].length; j++) { // showTimeLayout[0] can be any index, just need to get
+																	// the number of cols
+				// Print seat status
+				System.out.print(" [" + showTimeLayout[i][j].getSeatStatus() + "] ");
+			}
 
-            System.out.print("\n");
-        }
-    }
+			System.out.print("\n");
+		}
+	}
 
-    // Mark a seat as taken
-    public boolean bookSeat(String seatNum) {
-        for(int i=0; i < showTimeLayout.length; i++) {
-            for(int j=0; j < showTimeLayout[0].length; j++) {       // showTimeLayout[0] can be any index, just need to get the number of cols
-                if(showTimeLayout[i][j].getSeatNum().equals(seatNum))
-                    if(showTimeLayout[i][j].getSeatStatus() == 0) {
-                        showTimeLayout[i][j].setSeatStatus(1);
+	// Mark a seat as taken
+	public boolean bookSeat(String seatNum) {
+		for (int i = 0; i < showTimeLayout.length; i++) {
+			for (int j = 0; j < showTimeLayout[0].length; j++) { // showTimeLayout[0] can be any index, just need to get
+																	// the number of cols
+				if (showTimeLayout[i][j].getSeatNum().equals(seatNum))
+					if (showTimeLayout[i][j].getSeatStatus() == 0) {
+						showTimeLayout[i][j].setSeatStatus(1);
 
-                        return true;
-                    }
-            }
-        }
+						return true;
+					}
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public void setShowTimeLayout(Seat[][] showTimeLayout) {
-        this.showTimeLayout = showTimeLayout;
-    }
+	public void setShowTimeLayout(Seat[][] showTimeLayout) {
+		this.showTimeLayout = showTimeLayout;
+	}
 
-    public int getNumOfAvailSeats() {
-        return numOfAvailSeats;
-    }
+	public int getNumOfAvailSeats() {
+		return numOfAvailSeats;
+	}
 
-    public void setNumOfAvailSeats(int numOfAvailSeats) {
-        this.numOfAvailSeats = numOfAvailSeats;
-    }
+	public void setNumOfAvailSeats(int numOfAvailSeats) {
+		this.numOfAvailSeats = numOfAvailSeats;
+	}
 }

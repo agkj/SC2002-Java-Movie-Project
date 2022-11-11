da9e623d06bd7694b712164b4b0afa39542e1085
bchewzy <bchewzy@gmail.com>
2022-11-11 17:19:54 +0800

    fix top 5

--- src/Admin/TopFiveApp.java
+++ src/Admin/TopFiveApp.java
@@ -16,10 +16,38 @@ public class TopFiveApp extends AppHelper {
     @Override
     public void runInterface() {
         System.out.println("------- CONFIGURE MOVIE_GOER VIEWING OPTIONS -------\n");
+        System.out.print("Current setting in MovieGoer: ");
+        //look at the admin control
+        StringBuffer optionView = FileReader.copyFile(root + "control.txt");
+        String[] optionStrings = optionView.toString().split("\n");
+        int set_control_ticket = 0;
+        int set_control_rating = 0;
 
-        System.out.println("1) View by Ticket Sales");
-        System.out.println("2) View by Overall Reviewers' Rating");
-        System.out.println("3) View by Ticket Sales or View by Overall Reviewers' Rating");
+        for(int i=0; i < optionStrings.length; i++) {
+            String[] currLine = optionStrings[i].split(",");
+            String option = currLine[0];
+            String optionBoolean = currLine[1];
+
+            if(option.equals("Ticket Sales") && optionBoolean.equals("true")){
+                set_control_ticket = 1;
+
+            }else if(option.equals("Reviewers' Rating") && optionBoolean.equals("true")){
+                set_control_rating = 1;
+            }
+        }
+
+        if( set_control_ticket == 1 && set_control_rating == 1 ){
+            System.out.println("View by Ticket Sales and Overall Reviewers' Rating");
+        } else if (set_control_ticket == 1 && set_control_rating == 0 ){
+            System.out.println("View by Ticket Sales ONLY");
+        } else if (set_control_ticket == 0 && set_control_rating == 1){
+            System.out.println("View by Overall Reviewers' Rating ONLY");
+        }
+        System.out.println();
+        System.out.println("MovieGoer viewing options:");
+        System.out.println("1) View by Ticket Sales ONLY");
+        System.out.println("2) View by Overall Reviewers' Rating ONLY");
+        System.out.println("3) View by Ticket Sales and Overall Reviewers' Rating");
         System.out.println("\n0) Return to Previous Menu");
 
         System.out.println("----------------------------------------");
@@ -48,7 +76,7 @@ public class TopFiveApp extends AppHelper {
             case 3:
                 FileReader.replaceLine(root + "control.txt", "Ticket Sales", "Ticket Sales," + "true", false);
                 FileReader.replaceLine(root + "control.txt", "Reviewers' Rating", "Reviewers' Rating," + "true", false);
-                System.out.println("SYSTEM SETTING - REVIEWERS' RATING VIEW SUCCESSFULLY");
+                System.out.println("SYSTEM SETTING - TICKET SALES & REVIEWERS' RATING VIEW SUCCESSFULLY");
                 goBack().runInterface();
             default:
                 break;
